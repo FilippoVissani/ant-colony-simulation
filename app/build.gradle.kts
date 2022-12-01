17894884815ec55506212e8f9edf2c17b8ff6345
Filippo Vissani <filippo.vissani@studio.unibo.it>
2022-12-01 13:16:18 +0100

    chore: Configured kotest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,6 +19,21 @@ application {
     mainClass.set("io.github.filippovissani.acs.AppKt")
 }
 
+kotlin {
+    target {
+        compilations.all {
+            kotlinOptions {
+                allWarningsAsErrors = true
+                freeCompilerArgs = listOf("-opt-in=kotlin.RequiresOptIn")
+            }
+        }
+    }
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+}
+
 gitSemVer {
     buildMetadataSeparator.set("-")
     maxVersionLength.set(20)
---
+++ app/src/test/kotlin/io/github/filippovissani/acs/AppKtTest.kt
@@ -0,0 +1,10 @@
+package io.github.filippovissani.acs
+
+import io.kotest.assertions.fail
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class AppKtTest : FunSpec({
+
+    test("main") {}
+})

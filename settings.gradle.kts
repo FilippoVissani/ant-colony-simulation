7714984cad33e92686f3f65daca407d56fcbbc1e
Filippo Vissani <filippo.vissani@studio.unibo.it>
2022-12-01 12:20:48 +0100

    chore: Added gradle tasks

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,4 +8,16 @@
  */
 
 rootProject.name = "ant-colony-simulation"
+val destination = File(".git/hooks/commit-msg")
 include("app")
+
+plugins {
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "1.0.23"
+}
+
+gitHooks {
+    commitMsg {
+        conventionalCommits()
+    }
+    createHooks(true)
+}
